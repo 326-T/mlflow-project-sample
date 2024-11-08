664403a35397527702e37ee6538c604c8c1a097c
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-11-08 15:46:07 +0900

    [add]Confirmed preprocessing functionality up to this point

--- predictors/sentence-transformer/model.py
+++ predictors/sentence-transformer/model.py
@@ -1,31 +1,45 @@
 import argparse
 import logging
-from typing import Dict, List
+from typing import Any, AsyncIterator, Dict, List, Union
 
 from kserve import Model, ModelServer, model_server
 from kserve.model import PredictorConfig, PredictorProtocol
+from kserve.protocol.grpc.grpc_predict_v2_pb2 import ModelInferRequest
+from kserve.protocol.infer_type import InferRequest, InferResponse
 from mlflow.environment_variables import _EnvironmentVariable
 from mlflow.sentence_transformers import load_model
 from sentence_transformers import SentenceTransformer
 
 
 class SentenceTransformerModel(Model):
-    def __init__(self, name: str, model_uri: str, predictor_protocol: str):
+    def __init__(
+        self, name: str, model_uri: str, predictor_host: str, predictor_protocol: str
+    ):
         super().__init__(
             name,
-            PredictorConfig(predictor_protocol=predictor_protocol),
+            PredictorConfig(
+                predictor_host=predictor_host, predictor_protocol=predictor_protocol
+            ),
             return_response_headers=True,
         )
         self.model_uri = model_uri
         self.load()
 
     def load(self):
-        self.model: SentenceTransformer = load_model(model_uri=self.model_uri)
+        # self.model: SentenceTransformer = load_model(model_uri=self.model_uri)
+        self.model: SentenceTransformer = SentenceTransformer(
+            model_name_or_path="intfloat/multilingual-e5-large",
+        )
         self.model.eval()
         self.ready = True
         logging.info(f"Model loaded from {self.model_uri}")
 
-    def predict(self, payload: Dict[str, str] = None) -> Dict:
+    async def predict(
+        self,
+        payload: Union[Dict, InferRequest, ModelInferRequest],
+        headers: Dict[str, str] = None,
+        response_headers: Dict[str, str] = None,
+    ) -> Union[Dict, InferResponse, AsyncIterator[Any]]:
         """
         Embed text data using the SentenceTransformer model.
 
@@ -65,11 +79,13 @@ class SentenceTransformerModel(Model):
             ValueError: If the input payload does not contain valid text data.
         """
         try:
-            texts = self._extract_texts_v2(payload)
+            texts = self._extract_texts(payload)
         except ValueError as e:
             return {"error": str(e)}
         embeddings = self.model.encode(texts)
         result = {
+            "model_name": self.name,
+            "id": payload.get("id"),
             "outputs": [
                 {
                     "name": "embeddings",
@@ -77,15 +93,22 @@ class SentenceTransformerModel(Model):
                     "datatype": "FP32",
                     "data": embeddings.tolist(),
                 }
-            ]
+            ],
         }
         return result
 
-    def _extract_texts_v2(self, payload: Dict[str, str] = None) -> List[str]:
+    def _extract_texts(self, payload: Union[Dict, InferRequest] = None) -> List[str]:
+        if isinstance(payload, InferRequest):
+            return self._extract_texts_v2(payload)
+        else:
+            logging.error("Unsupported payload format.")
+            raise ValueError("Unsupported payload format.")
+
+    def _extract_texts_v2(self, payload: InferRequest = None) -> List[str]:
         """Extract text data from the input payload following the V2 protocol.
 
         Args:
-            payload (Dict[str, Any]): The input payload containing text data.
+            payload (InferRequest): The input payload containing text data.
                 It should follow the V2 protocol format with an "inputs" key.
                 Example:
                 {
@@ -111,7 +134,7 @@ class SentenceTransformerModel(Model):
                     "Another test."
                 ]
         """
-        inputs = payload.get("inputs", [])
+        inputs: List[InferRequest] = payload.inputs
         if not inputs:
             logging.error("入力データが見つかりません。")
             raise ValueError("No input data found in the payload.")
@@ -119,7 +142,7 @@ class SentenceTransformerModel(Model):
         # Extract text data from the input payload
         texts = []
         for input_data in inputs:
-            data = input_data.get("data")
+            data = input_data.to_dict().get("data")
             if data is None:
                 logging.warning("データが見つかりませんでした。")
                 continue
@@ -138,12 +161,7 @@ class SentenceTransformerModel(Model):
 
 MODELS_DIR = _EnvironmentVariable("MODELS_DIR", str, "/mnt/models")
 parser = argparse.ArgumentParser(parents=[model_server.parser])
-parser.add_argument(
-    "--model_name",
-    help="The name that the model is served under.",
-    default="sentence-transformer",
-)
-parser.add_argument("--protocol", help="The protocol for the predictor", default="v2")
+
 args, _ = parser.parse_known_args()
 
 if __name__ == "__main__":
@@ -151,6 +169,7 @@ if __name__ == "__main__":
     model = SentenceTransformerModel(
         name=args.model_name,
         model_uri=MODELS_DIR.get(),
+        predictor_host=args.predictor_host,
         predictor_protocol=PredictorProtocol(args.predictor_protocol),
     )
     ModelServer(workers=1).start([model])
