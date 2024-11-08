78a31c351466d0da783c0dbcdb06d01a66cf2dc8
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-11-08 17:13:08 +0900

    [fix]can inference using rest v1 and v2 protocol

--- predictors/sentence-transformer/model.py
+++ predictors/sentence-transformer/model.py
@@ -1,11 +1,11 @@
 import argparse
 import logging
-from typing import Any, AsyncIterator, Dict, List, Union
+from typing import Any, AsyncIterator, Dict, Union
 
 from kserve import Model, ModelServer, model_server
 from kserve.model import PredictorConfig, PredictorProtocol
 from kserve.protocol.grpc.grpc_predict_v2_pb2 import ModelInferRequest
-from kserve.protocol.infer_type import InferRequest, InferResponse
+from kserve.protocol.infer_type import InferOutput, InferRequest, InferResponse
 from mlflow.environment_variables import _EnvironmentVariable
 from mlflow.sentence_transformers import load_model
 from sentence_transformers import SentenceTransformer
@@ -26,10 +26,7 @@ class SentenceTransformerModel(Model):
         self.load()
 
     def load(self):
-        # self.model: SentenceTransformer = load_model(model_uri=self.model_uri)
-        self.model: SentenceTransformer = SentenceTransformer(
-            model_name_or_path="intfloat/multilingual-e5-large",
-        )
+        self.model: SentenceTransformer = load_model(model_uri=self.model_uri)
         self.model.eval()
         self.ready = True
         logging.info(f"Model loaded from {self.model_uri}")
@@ -40,72 +37,48 @@ class SentenceTransformerModel(Model):
         headers: Dict[str, str] = None,
         response_headers: Dict[str, str] = None,
     ) -> Union[Dict, InferResponse, AsyncIterator[Any]]:
-        """
-        Embed text data using the SentenceTransformer model.
+        if isinstance(payload, InferRequest):
+            return self._predict_rest_v2(payload)
+        elif isinstance(payload, ModelInferRequest):
+            return self._predict_grpc_v2(payload)
+        return self._predict_rest_v1(payload)
 
-        This method processes the input payload, extracts text data, and computes
-        embeddings for the provided text using a pre-trained SentenceTransformer model.
+    def _predict_rest_v1(self, payload: Dict) -> Dict:
+        """
+        Predicts the embeddings of the input text data using REST V1 protocol.
 
         Args:
-            payload (Dict[str, Any]): The input payload containing text data.
-                It should follow the V2 protocol format with an "inputs" key.
+            payload (Dict): The input payload containing text data.
+                It should follow the V1 protocol format with an "instances" key.
                 Example:
                 {
-                    "inputs": [
-                        {
-                            "name": "text",
-                            "shape": [2],
-                            "datatype": "BYTES",
-                            "data": ["This is a test.", "Another test."]
-                        }
+                    "instances": [
+                        "This is a test.",
+                        "Another test."
                     ]
                 }
 
         Returns:
-            Dict: The output payload containing embeddings in the V2 protocol format.
+            Dict: The response containing the embeddings of the input text data.
                 Example:
                 {
-                    "outputs": [
-                        {
-                            "name": "embeddings",
-                            "shape": [2, 1024],
-                            "datatype": "FP32",
-                            "data": [[...], [...]]  # Embedding vectors for each input text.
-                        }
-                    ]
+                    "predictions": [[0.1, 0.2, ..., 0.8], [0.3, 0.4, ..., 0.9]]
                 }
-
-        Raises:
-            ValueError: If the input payload does not contain valid text data.
         """
-        try:
-            texts = self._extract_texts(payload)
-        except ValueError as e:
-            return {"error": str(e)}
+        instances = payload.get("instances")
+        texts = []
+        for instance in instances:
+            if isinstance(instance, list):
+                texts.extend(instance)
+            elif isinstance(instance, dict) and "text" in instance:
+                texts.append(instance)
+
         embeddings = self.model.encode(texts)
-        result = {
-            "model_name": self.name,
-            "id": payload.get("id"),
-            "outputs": [
-                {
-                    "name": "embeddings",
-                    "shape": embeddings.shape,
-                    "datatype": "FP32",
-                    "data": embeddings.tolist(),
-                }
-            ],
-        }
-        return result
+        return {"predictions": embeddings.tolist()}
 
-    def _extract_texts(self, payload: Union[Dict, InferRequest] = None) -> List[str]:
-        if isinstance(payload, InferRequest):
-            return self._extract_texts_v2(payload)
-        else:
-            logging.error("Unsupported payload format.")
-            raise ValueError("Unsupported payload format.")
-
-    def _extract_texts_v2(self, payload: InferRequest = None) -> List[str]:
-        """Extract text data from the input payload following the V2 protocol.
+    def _predict_rest_v2(self, payload: InferRequest = None) -> InferResponse:
+        """
+        Predicts the embeddings of the input text data using REST V2 protocol.
 
         Args:
             payload (InferRequest): The input payload containing text data.
@@ -122,41 +95,49 @@ class SentenceTransformerModel(Model):
                     ]
                 }
 
-        Raises:
-            ValueError: If the input payload does not contain valid text data.
-            ValueError: If the input payload does not contain any data.
-
         Returns:
-            List[str]: A list of text data extracted from the input payload.
+            InferResponse: The response containing the embeddings of the input text data.
                 Example:
-                [
-                    "This is a test.",
-                    "Another test."
-                ]
+                {
+                    "id": "1",
+                    "model_name": "sentence-transformer",
+                    "model_version": "1.0",
+                    "parameters": {},
+                    "outputs": [
+                        {
+                            "name": "embeddings",
+                            "shape": [2, 1024],
+                            "datatype": "FP32",
+                            "parameters": {},
+                            "data": [[0.1, 0.2, ..., 0.8], [0.3, 0.4, ..., 0.9]]
+                        }
+                    ]
+                }
         """
-        inputs: List[InferRequest] = payload.inputs
-        if not inputs:
-            logging.error("入力データが見つかりません。")
-            raise ValueError("No input data found in the payload.")
-
-        # Extract text data from the input payload
         texts = []
-        for input_data in inputs:
+        for input_data in payload.inputs:
             data = input_data.to_dict().get("data")
-            if data is None:
-                logging.warning("データが見つかりませんでした。")
-                continue
             if isinstance(data, list):
                 texts.extend(data)
             elif isinstance(data, str):
                 texts.append(data)
-            else:
-                logging.warning(f"無効なデータ形式: {data}")
 
-        if not texts:
-            logging.error("有効なテキストデータがありません。")
-            raise ValueError("No valid text data found in the payload.")
-        return texts
+        embeddings = self.model.encode(texts)
+        return InferResponse(
+            response_id=payload.id or "none",
+            model_name=self.name,
+            infer_outputs=[
+                InferOutput(
+                    name="embeddings",
+                    shape=embeddings.shape,
+                    datatype="FP32",
+                    data=embeddings.tolist(),
+                )
+            ],
+        )
+
+    def _predict_grpc_v2(self, payload: ModelInferRequest) -> AsyncIterator[Any]:
+        raise NotImplementedError("gRPC V2 protocol is not supported.")
 
 
 MODELS_DIR = _EnvironmentVariable("MODELS_DIR", str, "/mnt/models")
