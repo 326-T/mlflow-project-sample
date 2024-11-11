a629eca4876db140bac4615124953964e75ae297
Mitsuru Takeda <m.t.hagaren1@gmail.com>
2024-11-11 09:01:15 +0900

    [add]sample metrics and params

--- projects/multilingual-e5-large/main.py
+++ projects/multilingual-e5-large/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from mlflow import start_run
+from mlflow import start_run, log_param, log_metric
 from mlflow.models import infer_signature
 from mlflow.models.signature import ModelSignature
 from mlflow.sentence_transformers import log_model
@@ -17,9 +17,12 @@ signature: ModelSignature = infer_signature(
 )
 
 with start_run():
+    log_param("model_name", "intfloat/multilingual-e5-large")
+    log_param("batch_size", 64)
     log_model(
         model=model,
         artifact_path="multilingual-e5-large",
         signature=signature,
         input_example=sample_input,
     )
+    log_metric("loss", 0.1)
